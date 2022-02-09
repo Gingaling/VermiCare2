c1ab60046c18d9179592f3da47e3a2cfbe04212d
gingaling <nycgingsf@hotmail.com>
2022-02-09 14:50:51 -0800

    Committing files.

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,23 @@ console.log("%c[scripts/app.js] Loaded", "background:orange; padding:10px");
 
 $('button').on('click', setUpWormWorld());
 
-class Worm {
-    constructor () {
-        this.name = name;
-        this.hunger = hunger;
-        this.sleepiness = sleepiness;
-        this.boredom = boredom;
-        this.naughtiness = naughtiness;
-    }
 
-    // method1
-    // methodd2
-}
+// class Worm {
+//     constructor () {
+//         this.name = name;
+//         this.hunger = hunger;
+//         this.sleepiness = sleepiness;
+//         this.boredom = boredom;
+//         this.naughtiness = naughtiness;
+//     }
+
+//     // method1
+//     // methodd2
+// }
 
 function setUpWormWorld() {
-// new background
-// instatiate work
+    console.log("Hello, after click");
+    $('#worm-world-opener').appendChild($('section');
+// // new background
+// // instatiate work
 }
