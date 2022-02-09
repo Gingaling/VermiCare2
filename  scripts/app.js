ba09c3c4f76b34869e1b9db90f5c4b048ebdc52a
gingaling <nycgingsf@hotmail.com>
2022-02-09 11:36:13 -0800

    Committing files after resolving major blocker. (Only through aid of Esin.

---  scripts/app.js
+++
@@ -1,7 +0,0 @@
-console.log("%c[scripts/app.js] Loaded", "background:orange; padding:10px");
-
-function sayHello() {
-    console.log("Hello");
-}
-
-$('button').on('click', sayHello());
---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+// console.log("%c[scripts/app.js] Loaded", "background:orange; padding:10px");
+
+// $('button').on('click', setUpWormWorld());
+console.log("Hello");
+// class Worm {
+//     constructor () {
+//         this.name = name;
+//         this.hunger = hunger;
+//         this.sleepiness = sleepiness;
+//         this.boredom = boredom;
+//         this.naughtiness = naughtiness;
+//     }
+
+//     // method1
+//     // methodd2
+// }
+
+// function setUpWormWorld() {
+// // new background
+// // instatiate work
+// }
