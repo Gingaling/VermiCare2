5c23242158f86977baf38716a794acf6e85bb005
Gingaling <nycgingsf@hotmail.com>
2022-02-10 07:43:37 -0800

    Committing functionality code.

--- scripts/app.js
+++ scripts/app.js
@@ -3,22 +3,30 @@ console.log("%c[scripts/app.js] Loaded", "background:orange; padding:10px");
 $('button').on('click', setUpWormWorld());
 
 
-// class Worm {
-//     constructor () {
-//         this.name = name;
-//         this.hunger = hunger;
-//         this.sleepiness = sleepiness;
-//         this.boredom = boredom;
-//         this.naughtiness = naughtiness;
-//     }
+class Worm {
+    constructor () {
+        this.name = name;
+        this.age = age
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
+        this.naughtiness = naughtiness;
+    }
 
-//     // method1
-//     // methodd2
+    // method1
+    // methodd2
+}
+
+function getPetName {
+    const petName = prompt("Please let us know what you would like to call your pet worm.")
+}
+
+function createWorm {
+    const newWorm = new Worm
+}
+
+// function setUpWormWorld() {
+//     console.log("Hello, after click");
 // }
 
-function setUpWormWorld() {
-    console.log("Hello, after click");
-    $('#worm-world-opener').appendChild($('section');
-// // new background
-// // instatiate work
-}
+// $('#worm-world').appendChild($('section'));
