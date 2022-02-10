1c0c475f3b984335addfb682e65414f538307015
Gingaling <nycgingsf@hotmail.com>
2022-02-10 08:21:34 -0800

    Committing additional functionalitycode.

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,54 @@ console.log("%c[scripts/app.js] Loaded", "background:orange; padding:10px");
 
 $('button').on('click', setUpWormWorld());
 
+/* Round of Vermicare */
+const round = {
+    name: null,
+    age: 0,
+    hunger: 0,
+    sleepiness: 0,
+    boredom: 0,
+    naughtiness:
+    button: [ 
+        $("enterName"),
+        $("#feed me"),
+        $("let sleep"),
+        $("#play with me"),
+        $("#read to me")
+    ],
+}
+
+start(e){
+    this.startAgeTimer();
+    this.startHungerTimer();
+    this.startSleepinessTimer();
+    this.startBoredomTimer();
+    this.startNaughtinessTimer();
+},
+
+
+/* === Timers for Hunger, Age, Sleepiness, Boredom and Naughtiness
+=== */
+ageTimer: null,
+startAgeTimer() {
+    this.ageTimer = setInterval(this.increaseAge.bind(round), 6000)
+},
+
+hungerTimer: null,
+startHungerTimer() {
+    this.hungerTimer = setInterval(this.increaseHunger.bind(game), 1250)
+},
+
+
+sleepinessTimer: null,
+startSleepyTimer() {
+    this.sleepyTimer = setInterval(this.increaseSleepiness.bind(game), 2500)
+},
+
+naughtinessTimer: null,
+startBoredomTimer() {
+    this.boredomTimer = setInterval(this.increaseBoredom.bind(game), Math.floor(Math.random() * 1000))
+},
 
 class Worm {
     constructor () {
